cb102dac2f22726d2d99b3b80a3585617a9e7cbc
Jian Beerens <Beerensjian@gmail.com>
2023-12-02 02:32:31 +0100

    Advent of Code day1

---
+++ day1/part2/index.js
@@ -0,0 +1,75 @@
+import { readFile } from 'node:fs';
+
+//read values from file
+let strings = [];
+readFile('./strings.txt', 'utf-8', (err, data) => {
+    // error converting file
+    if (err) {
+        console.error(err)
+        return
+    }
+    // convert the string into an array of strings
+    strings = data.split('\r\n');
+
+    // push the numbers into final array
+    let finalNumbers = [];
+    strings.forEach(str => {
+        const convertedStr = ConvertTextNumbersToNumbersInString(str)
+        finalNumbers.push(getFirstAndLastInt(convertedStr));
+    })
+
+
+    // count all the numbers together
+    const sumOfNumbers = finalNumbers.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue
+    }, 0)
+
+    console.log(sumOfNumbers)
+})
+
+const numberStrings = Object.freeze({
+    'one' : 1,
+    'two' : 2,
+    'three' : 3,
+    'four' : 4,
+    'five' : 5,
+    'six' : 6,
+    'seven' : 7,
+    'eight' : 8,
+    'nine' : 9
+})
+
+// returns the first and last number appended together and converted to integer
+function getFirstAndLastInt(str) {
+    const stringArr = str.split('');
+    let numbers = [];
+    stringArr.forEach(element => {
+        if (!isNaN(parseInt(element))) {
+            numbers.push(element);
+        }
+    });
+    return parseInt(numbers[0] + numbers[numbers.length - 1]);
+}
+
+
+function ConvertTextNumbersToNumbersInString(str) {
+    let indexesOfStringNumbers = [];
+    let updatedString = str;
+    
+    // find all the occurences of strings that represent a number, return those occurences as [startIndex, endIndex, value]
+    for(const [key, value] of Object.entries(numberStrings)) {
+
+        while (updatedString.indexOf(key) != -1) {
+            const startIndex = updatedString.indexOf(key);
+            const endIndex = updatedString.indexOf(key) + key.length - 1;
+            const firstPart = updatedString.slice(0, startIndex + 1);
+            const lastPart = updatedString.slice(endIndex);
+            updatedString = firstPart + value + lastPart;
+        }
+    }
+    
+    return updatedString;
+}
+
+
+
